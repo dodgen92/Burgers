142d1b470949debbc9e123a0b69815c30f3f969a
Tyler Dodgen <tylerremingtondodgen@gmail.com>
2020-04-26 07:30:11 -0400

    added burgers to seeds

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,13 @@
-// Pull in required dependencies
+// dependencies
 var mysql = require('mysql');
 
-// Create the MySQL connection object
 var connection;
-
+//if else for local run vs heroku
 if (process.env.JAWSDB_URL) {
-	// DB is JawsDB on Heroku
+	
 	connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
-	// DB is local on localhost
+	
 	connection = mysql.createConnection({
 		port: 3306,
 		host: 'localhost',
@@ -18,7 +17,7 @@ if (process.env.JAWSDB_URL) {
 	})
 };
 
-// Make the connection to MySQL
+
 connection.connect(function(err) {
   if (err) {
     console.error('ERROR: MySQL connection error -- ' + err.stack + '\n\n');
@@ -27,5 +26,5 @@ connection.connect(function(err) {
   console.log('Connected to MySQL database as id ' + connection.threadId + '\n\n');
 });
 
-// Export connection for ORM use
+
 module.exports = connection;
--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-// Pull in required dependencies
+// required dependencies
 var express = require('express');
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
@@ -6,15 +6,14 @@ var port = process.env.PORT || 3000;
 
 var app = express();
 
-// Serve static content for the app from the 'public' directory
+
 app.use(express.static(process.cwd() + '/public'));
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
-// Override with POST having ?_method=DELETE
 app.use(methodOverride('_method'));
 
-// Set Handlebars as the view engine
+
 var exphbs = require('express-handlebars');
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
