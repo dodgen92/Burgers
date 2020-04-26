942280bd194b5522212b444a200375905023d3a3
Tyler Dodgen <tylerremingtondodgen@gmail.com>
2020-04-26 05:47:54 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Burgers
+ A burger simulator app created with Node.js/Express/MySQL/Handlebars/Materialize
---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+-- Creating seed data for the burgers_db database --
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Bleu Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Shrimp and chicken Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Veggie Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Murder Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cow Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Whiskey Burger', true);
